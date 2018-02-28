f5bfe438825fe5009d5179a2ac10b6086ec1b036
QaysTrad <qaystrad@gmail.com>
2018-02-28 20:33:58 +0200

    QT

--- Questions/questions.js
+++ Questions/questions.js
@@ -1,7 +1,18 @@
+var divv = $("<div> </div>")
 var first= $("<p>HI ! </p>");
-
+var btnNext = $("<button></button")
 var name = localStorage.getItem("lastname")
+var radio = localStorage.getItem("Radio")
+
+divv.append(first).append(btnNext)
 
 first.append(name)
+divv.addClass("firstext")
+btnNext.addClass("btn btn-defult")
+$("body").append(divv)
 
-$("body").append(first)
+btnNext.on("click",function () {
+	$(".firstext").fadeOut();
+})
+
+/************************/
