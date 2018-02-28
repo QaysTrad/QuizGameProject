56bae8c951c73c7bfa531aa26a14b517d13e713c
Belalfaouri <belalfaourig@gmail.com>
2018-02-28 15:31:19 +0200

    second array incomplete

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ subject.sports={
   fakeAnswer3:[]
 };
 subject.games={
-  questions:[],
-  answers:[],
-  fakeAnswer1:[],
-  fakeAnswer2:[],
-  fakeAnswer3:[]
+  questions:['World of Warcraft:What is the name of the desert that surrounds the Caverns of Time?','World of Warcraft:What pit lord was the ruler of Outland before being imprisoned by Illidan Stormrage?'],
+  answers:['Tanaris','Magtheradon'],
+  fakeAnswer1:['Silithus','Azgalor'],
+  fakeAnswer2:['Desolace','Kazzak'],
+  fakeAnswer3:['Uldum','Grax']
 };
 subject.tv={
   questions:['Ser Jaime Lannister is often referred to as...','What is the name of the leader of the Unsullied?','Which character says the line: "The Lannisters send their regards"?',' Who here releases the demon who is destined to break the 66 seals and free Lucifer?','Which of the four horsemen of the apocalypse does not get his ring taken away from him by either Sam, Dean or Castiel?','Compared to a bear and lives in the shadow of his brother..','Can forsee future events..','Who said: "Ideals are peaceful. History is violent"?','What a deadpool\'s favourite weapons?','What is the mental illness in I Am Sam?'],
